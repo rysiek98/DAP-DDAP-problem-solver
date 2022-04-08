efaa1f4e4843f4ad818a13f361d9c116221d1762
rysiek98 <m.ryszka98@gmail.com>
2022-04-08 18:17:17 +0200

    add package

---
+++ src/pl/pw/Network.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Network {
+
+    private List<Link> linkList;
+    private List<Demand> demandList;
+
+
+    public Network() { }
+
+    public Network(List<Link> linkList, List<Demand> demandList) {
+        this.linkList = linkList;
+        this.demandList = demandList;
+    }
+}
---
+++ src/pl/pw/Path.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Path {
+    private int id; // nie wiem czy to jest potzebne
+    private List<Link> linkList;
+
+    public int getId() {
+        return id;
+    }
+
+    public List<Link> getLinkList() {
+        return linkList;
+    }
+}
